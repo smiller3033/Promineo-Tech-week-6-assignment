3eecd3532cd84227591faa3c242fc9e9727c7c10
Stephen Miller <smiller2000@comcast.net>
2022-11-17 13:54:55 -0500

    finished first draft of game, still needs debugging, imported chai and mocha

--- WAR.js
+++ WAR.js
@@ -15,8 +15,51 @@ function shuffle(array) {
     }
   
     return array;
-  }
-  
+}
+
+//defines the face of a playing card
+function defineFace(value) {
+    if (value === 2) {
+        return "Two";
+    }
+    else if (value === 3) {
+        return "Three";
+    }
+    else if (value === 4) {
+        return "Four";
+    }
+    else if (value === 5) {
+        return "Five";
+    }
+    else if (value === 6) {
+        return "Six";
+    }
+    else if (value === 7) {
+        return "Seven";
+    }
+    else if (value === 8) {
+        return "Eight";
+    }
+    else if (value === 9) {
+        return "Nine";
+    }
+    else if (value === 10) {
+        return "Ten";
+    }
+    else if (value === 11) {
+        return "Jack";
+    }
+    else if (value === 12) {
+        return "Queen";
+    }
+    else if (value === 13) {
+        return "King";
+    }
+    else if (value === 14) {
+        return "Ace";
+    }
+}  
+
 //This is a singular playing card
 class Card {
     constructor(value, suit) {
@@ -24,55 +67,12 @@ class Card {
         this.suit = suit;
         this.face = defineFace(value);
     }
-
-    //Defines the face of the card by the given value
-    defineFace(value) {
-        if (value === 2) {
-            return "Two";
-        }
-        else if (value === 3) {
-            return "Three";
-        }
-        else if (value === 4) {
-            return "Four";
-        }
-        else if (value === 5) {
-            return "Five";
-        }
-        else if (value === 6) {
-            return "Six";
-        }
-        else if (value === 7) {
-            return "Seven";
-        }
-        else if (value === 8) {
-            return "Eight";
-        }
-        else if (value === 9) {
-            return "Nine";
-        }
-        else if (value === 10) {
-            return "Ten";
-        }
-        else if (value === 11) {
-            return "Jack";
-        }
-        else if (value === 12) {
-            return "Queen";
-        }
-        else if (value === 13) {
-            return "King";
-        }
-        else if (value === 14) {
-            return "Ace";
-        }
-    }
 }
 
 //Deck of playing cards
 class Deck {
     constructor() {
-        this.cards = initializeDeck();
+        this.cards = [];
     }
 
     initializeDeck() {
@@ -82,7 +82,7 @@ class Deck {
         for (let i = 0; i < 4; i++) {
             suit = suits[i];
             for (let j = 2; j < 15; j++) {
-                cards.push(Card(j, suit));
+                cards.push(new Card(j, suit));
             }
         }
         shuffle(cards);
@@ -95,28 +95,82 @@ class Player {
     constructor(name) {
         this.name = name;
         this.playingCards = [];
+        this.score = 0;
     }
 }
 
 //Game of WAR
 class War {
     constructor() {
-        this.player1 = Player("Jeff");
-        this.player2 = Player("Martha");
-        this.deck = Deck();
+        this.player1 = new Player("Jeff");
+        this.player2 = new Player("Martha");
+        this.deck = new Deck();
+        this.deck.initializeDeck();
     }
 
     deal() {
         let card1;
         let card2;
         for (let i = 0; i < 26; i++) {
-            card1 = deck.cards.pop();
-            card2 = deck.cards.pop();
+            card1 = this.deck.cards.pop();
+            card2 = this.deck.cards.pop();
 
             this.player1.playingCards.push(card1);
             this.player2.playingCards.push(card2);
         }
     }
 
-    
+    play() {
+        let card1 = this.player1.playingCards.pop();
+        let card2 = this.player2.playingCards.pop();
+        let msg = `Player 1 plays ${card1.face} of ${card1.suit}
+        Player 2 plays ${card2.face} of ${card2.suit}
+        `
+
+        if (card1.value > card2.value) {
+            this.player1.score++;
+            return msg + "Player 1 Wins Round"
+        }
+        else if (card1.value < card2.value) {
+            this.player2.score++;
+            return msg + "Player 2 Wins Round"
+        }
+        else {
+            return msg + "Tie Round"
+        }
+    }
+
+    end() {
+        if (this.player1.score > this.player2.score) {
+            return "Player 1 Wins!"
+        }
+        else if (this.player1.score < this.player2.score) {
+            return "Player 2 Wins!"
+        }
+        else {
+            return "Tie!"
+        }
+    }
 }
+
+let card = new Card(14, "Spades");
+
+console.log(card.face);
+
+/*
+//initialize game and deal
+let game = new War();
+game.deal()
+
+//play game
+let gameMsg;
+for (let i = 0; i < 26; i++) {
+    console.log("ROUND " + (i + 1));
+    gameMsg = game.play();
+    console.log(gameMsg);
+}
+
+//determine winner
+results = game.end();d
+console.log(results);
+*/
